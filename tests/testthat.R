c8b01f0780e40683abb8e0539959ca5a5260c9f2
js2264 <jacquesserizay@gmail.com>
2022-08-17 14:16:00 +0200

    add tests vignette and doc

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(HiContactsData)
+test_check("HiContactsData")
---
+++ tests/testthat/test-HiContactsData.R
@@ -0,0 +1,19 @@
+context("test-HiContactsData")
+
+test_that("HiContactsData function works for yeast_wt", {
+    expect_equal({
+        s <- HiContactsData(sample = 'yeast_wt', format = 'cool')
+        methods::is(s, "character")
+    }, TRUE), 
+    expect_equal({
+        s <- HiContactsData(sample = 'yeast_wt', format = 'cool')
+        file.exists(s)
+    }, TRUE)
+})
+
+test_that("HiContactsDataFiles", {
+    expect_equal({
+        s <- HiContactsDataFiles()
+        methods::is(s, "data.frame")
+    }, TRUE)
+})
